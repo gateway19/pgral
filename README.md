863be0f6bd6c54e6a760d9614baeda817fd1a80d
admin19 <rxsend1911@yandex.ru>
2026-01-31 22:46:53 +0500

    add update support

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ pgral (Photo gallery grid app local)
 Download lastest [release](https://github.com/gateway19/pgral/releases/)  \
 save to folder \
 Create shortcut  PROGRAM_PATH/pgral.exe -u "http://127.0.0.1:8095/?path=C:\Users\username\Downloads\&regex=.*\.(png|jpg|jpeg)$" \
-or to PROGRAM_PATH/updater.exe -u "http://127.0.0.1:8095/?path=C:\Users\username\Downloads\&regex=.*\.(png|jpg|jpeg)$"  for auto update\
+or to PROGRAM_PATH/updater.exe -u "http://127.0.0.1:8095/?path=C:\Users\username\Downloads\&regex=.*\.(png|jpg|jpeg)$" -v main for auto update \
 
 
 # dev 
@@ -14,8 +14,8 @@ or to PROGRAM_PATH/updater.exe -u "http://127.0.0.1:8095/?path=C:\Users\username
 git clone https://github.com/gateway19/pgral/ --branch dev  
 cd pgral 
 pip install -r requirements.txt 
-python main.py   :: updater.exe
+python main.py 
 
 pyinstaller --onefile --add-data "templates;templates" --name pgral --hidden-import=uvicorn.protocols.http.h11_impl --hidden-import=uvicorn.protocols.websockets.websockets_impl main.py 
-go build -o updater.exe main.go
+go build -o dist/updater.exe main.go
 ```
--- main.go
+++ main.go
@@ -1,18 +1,63 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"os/exec"
 	"path/filepath"
 )
 
-func update(ver string) { // do later
+func checksum(url, app string) (string, string, string) {
+	// local_sha
+	f, _ := os.Open(app)
+	h := sha256.New()
+	io.Copy(h, f)
+	f.Close()
+	local_sha := hex.EncodeToString(h.Sum(nil))
+
+	// remote_sha
+	resp, err := http.Get(url)
+	if err != nil {
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+	var m map[string]string
+	if err := json.NewDecoder(resp.Body).Decode(&m); err != nil {
+		os.Exit(1)
+	}
+	remote_sha := m["sha256"]
+	return local_sha, remote_sha, m["version"]
+}
+
+func update(ver, app string) { // do later
+	var loc, rem, rver string
 	if ver == "main" {
-		//main
+		loc, rem, rver = checksum("https://raw.githubusercontent.com/gateway19/pgral/refs/heads/main/info.json", app)
 	} else {
-		// dev
+		loc, rem, rver = checksum("https://raw.githubusercontent.com/gateway19/pgral/refs/heads/dev/info.json", app)
+	}
+	if loc != rem {
+		// update
+		update_url := fmt.Sprintf("https://github.com/gateway19/pgral/releases/download/%s/pgral.exe", rver)
+		resp, err := http.Get(update_url)
+		if err != nil {
+			return
+		}
+		defer resp.Body.Close()
+
+		out, err := os.Create(app)
+		if err != nil {
+			return
+		}
+		defer out.Close()
+
+		io.Copy(out, resp.Body)
 	}
 }
 
@@ -20,10 +65,12 @@ func main() {
 	u := flag.String("u", "", "URL")
 	v := flag.String("v", "", "beta/main")
 	flag.Parse()
-	if *v == "main" || *v == "beta" {
-		update(*v)
-	}
 	pgralPath := filepath.Join(filepath.Dir(os.Args[0]), "pgral.exe")
 	u_argument := fmt.Sprintf("\"%s\"", *u)
+
+	if *v == "main" || *v == "beta" {
+		update(*v, pgralPath)
+	}
+
 	exec.Command("cmd", "/c", "start", "", pgralPath, "-u", u_argument).Start()
 }
